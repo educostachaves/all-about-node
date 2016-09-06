4daf4f801d12e8f482590a98b134cf1bc9453d53
Eduardo Chaves <eduardo.chaves@activesales.com.br>
2016-09-06 20:38:26 -0300

    implement webserver with promises and stream control

--- node-fundamentals/04-web-server/WebServer.js
+++ node-fundamentals/04-web-server/WebServer.js
@@ -13,48 +13,9 @@ let mimes = {
 	'.png': 'image/png'
 }
 
-// let routes = {
-// 	'GET': {
-// 		'/': (req, res) => {
-// 			res.writeHead(200, {'content-type': 'text/html'});
-// 			res.end('<h1>Hello Router</h1>');
-// 		},
-// 		'/about': (req, res) => {
-// 			res.writeHead(200, {'content-type': 'text/html'});
-// 			res.end('<h1>About page</h1>');
-// 		},
-// 		'/api/getinfo': (req, res) => {
-// 			res.writeHead(200, {'content-type': 'text/html'});
-// 			res.end(JSON.stringify(req.queryParams));
-// 		},
-// 	},
-// 	'POST': {
-// 		'/api/login': (req, res) => {
-// 			let body = '';
-// 			req.on('data', data => {
-// 				body += data;
-// 				if(body.length > 2097152) {
-// 					res.writeHead(413, {'content-type': 'text/html'});
-// 					res.end('<h3>Error: The file being uploaded exceeds the 2MB limit</h3>',
-// 						() => req.connection.destroy());
-// 				}
-// 			});
-
-// 			req.on('end', () => {
-// 				let params = qs.parse(body);
-// 				console.log('username:', params['username']);
-// 				console.log('password:', params['password']);
-// 				res.end();
-// 			});
-// 		}
-// 	},
-// 	'NA':(req, res) => {
-// 		res.writeHead(404);
-// 		res.end('Content not found!');
-// 	}
-// }
-
 function webserver(req, res) {
+	// if the route requested is '/', then load 'index.htm' or else
+	// load the requested file(s)
 	let baseURI = url.parse(req.url, true);
 	let filepath = __dirname + (baseURI.pathname === '/' ? '/index.htm' : baseURI.pathname);
 
@@ -81,14 +42,6 @@ function webserver(req, res) {
 			res.end('Content not found!');
 		}
 	});
-
-	/*let resolveRoute = routes[req.method][baseURI.pathname];
-	if(resolveRoute != undefined) {
-		req.queryParams = baseURI.query;
-		resolveRoute(req,res);
-	} else {
-		routes['NA'](req,res);
-	}*/
 }
 
 http.createServer(webserver).listen(3000, () => {
---
+++ node-fundamentals/04-web-server/WebServerProStream.js
@@ -0,0 +1,61 @@
+'use strict';
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+let mimes = {
+	'.htm': 'text/html',
+	'.css': 'text/css',
+	'.js': 'text/javascript',
+	'.jpg': 'image/jpeg',
+	'.gif': 'image/gif',
+	'.png': 'image/png'
+}
+
+function fileAcess(filepath) {
+	return new Promise((resolve, reject) => {
+		fs.access(filepath, fs.F_PK, error => {
+			if(!error) {
+				resolve(filepath);
+			} else {
+				reject(error);
+			}
+		})
+	});
+}
+
+function streamFile(filepath){
+	return new Promise((resolve, reject) => {
+		let fileStream = fs.createReadStream(filepath);
+
+		fileStream.on('open', () => {
+			resolve(fileStream);
+		});
+
+		fileStream.on('error', () => {
+			reject(error);
+		});
+	});
+}
+
+function webserver(req, res) {
+	// if the route requested is '/', then load 'index.htm' or else
+	// load the requested file(s)
+	let baseURI = url.parse(req.url, true);
+	let filepath = __dirname + (baseURI.pathname === '/' ? '/index.htm' : baseURI.pathname);
+	let contentType = mimes[path.extname(filepath)] //mimes['.css'] === 'text/css'
+
+	fileAcess(filepath)
+		.then(fileReader)
+		.then(content => {
+			res.writeHead(200, {'content-type': contentType});
+			res.end(content, 'utf-8');
+		})
+		.catch(error => {
+			res.writeHead(500);
+			res.end(JSON.stringify(error));
+		});
+}
+
+http.createServer(webserver).listen(3000, () => console.log('WebServer running on port 3000'));
